acd3cb4bc510c1b8ad60e61bb2d3c2cf7d5f69b4
shivamttl <shivamttl@gmail.com>
2021-06-29 00:29:26 +0530

    bug fix

--- payment/payment.js
+++ payment/payment.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false, })); //to provise all the warnings that we get
 app.set("view engine", "ejs");
-app.get("/payment", (req, res) => {
+app.get("/payments", (req, res) => {
   res.render("payment", { key: process.env.KEY_ID });
 });
 app.post("/api/payment/order", (req, res) => {
